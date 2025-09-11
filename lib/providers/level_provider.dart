a5a871a795a7b770c3c8df56cbc5e006ccd27e67
Ali Ahmed <eliehmed908@gmail.com>
2025-09-11 19:07:36 +0500

    Added level persistance using shared preferences

--- lib/providers/level_provider.dart
+++ lib/providers/level_provider.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LevelProvider extends ChangeNotifier {
   int _level = 1;
 
   int get level => _level;
 
+  LevelProvider() {
+    loadLevel();
+  }
+
+  // LOAD LEVEL FROM SHARED PREFERENCES
+  void loadLevel() async {
+    final prefs = await SharedPreferences.getInstance();
+    _level = prefs.getInt("currentLevel") ?? 1;
+    notifyListeners();
+  }
+
   // increment level
-  void incrementLevel() {
+  void incrementLevel() async {
     _level++;
     notifyListeners();
+
+    // Update current level in shared preferences
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('currentLevel', _level);
   }
 }
